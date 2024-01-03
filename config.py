dd551aafe9b7703d8376a4b482742a9923b1645f
Harighs <harishankarghs@gmail.com>
2024-01-03 13:50:54 +0100

    organisation for config.py

--- config.py
+++ config.py
@@ -1,19 +1,21 @@
 # /home/hari/projects/NSD_Project/config.py
 
 class Config:
+
+# Model Configuration
     input_dim = 16000
     output_dim = (256, 256)  # Height, Width
+    subject_to_use = (1,)
+    data_path = '/media/hari/2TB_T7/DATASET/FMRI/NSD-Furkan_Ozcelik/brain-diffuser/data/processed_data/'
+
+# Hyperparameters
     learning_rate = 0.001
     batch_size = 32
     num_epochs = 10
-    subject_to_use = (1,)
-    data_path = '/media/hari/2TB_T7/DATASET/FMRI/NSD-Furkan_Ozcelik/brain-diffuser/data/processed_data/'
+   
+# Training Configuration
+    data_splitting_ratio = 0.8
+    normalize_fmri = True
     Save_Model = True
     Saving_Model_Path = 'Saved_Models/'
-    normalize_fmri = True
-    data_splitting_ratio = 0.8
     
-    # Normal details (Not Changable)
-    training_start_time = None
-    training_end_time = None
-    total_time_taken = None
